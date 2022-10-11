f68934d15efa0307d6b45051064d773b6c16962e
Daniele Irsuti <irsooti+code@gmail.com>
2022-10-12 00:27:51 +0200

    feat: add routes

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,5 +1,16 @@
-import { fetch, json, type LoaderFunction } from '@remix-run/node';
-import { useLoaderData } from '@remix-run/react';
+import {
+  type ActionFunction,
+  fetch,
+  json,
+  redirect,
+  type LoaderFunction,
+} from '@remix-run/node';
+import {
+  Form,
+  useActionData,
+  useLoaderData,
+  useTransition,
+} from '@remix-run/react';
 import type AwesomeRemoteJob from '~/types/AwesomeRemoteJob';
 
 export const loader: LoaderFunction = async () => {
@@ -9,8 +20,45 @@ export const loader: LoaderFunction = async () => {
   return json(data);
 };
 
+export const action: ActionFunction = async ({ request, context, params }) => {
+  if (request.method === 'POST') {
+    const body = await request.formData();
+
+    const result = await new Promise((resolve) => {
+      setTimeout(() => {
+        const firstName = body.get('firstName');
+        const lastName = body.get('lastName');
+
+        if (firstName && lastName) resolve(true);
+
+        resolve(false);
+      }, 5000);
+    });
+
+    if (result) {
+      return redirect('/policies');
+    } else {
+      return json(
+        {
+          values: {
+            firstName: body.get('firstName'),
+            lastName: body.get('lastName'),
+          },
+          errors: {
+            all: 'Something went wrong',
+          },
+        },
+        400
+      );
+    }
+  }
+};
+
 export default function Index() {
   const data = useLoaderData<AwesomeRemoteJob[]>();
+  const actionData = useActionData();
+  const transition = useTransition();
+
   return (
     <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}>
       <ul>
@@ -18,6 +66,16 @@ export default function Index() {
           <li key={m.name}>{m.name}</li>
         ))}
       </ul>
+      <div>
+        <div>{actionData?.errors?.all}</div>
+        <Form method="post">
+          <input type="text" name="firstName" />
+          <input type="text" name="lastName" />
+          <button type="submit">
+            {transition.state === 'submitting' ? 'Creating...' : 'Create'}
+          </button>
+        </Form>
+      </div>
     </div>
   );
 }
