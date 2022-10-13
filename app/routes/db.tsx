7ec3261ae3843b78f6bc2779d86b6ee28c066c25
Dennis Alessandrini <dennisalessandrini@gmail.com>
2022-10-13 18:02:15 +0200

    zod form validation test, company creation 🤯

--- app/routes/db.tsx
+++ app/routes/db.tsx
@@ -1,7 +1,13 @@
-import { type Company } from "@prisma/client";
-import { json, type LoaderFunction } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
+import { RemotePolicy, type Company } from "@prisma/client";
+import {
+  type ActionFunction,
+  json,
+  redirect,
+  type LoaderFunction,
+} from "@remix-run/node";
+import { Form, useActionData, useLoaderData } from "@remix-run/react";
 import { db } from "~/lib/db.server";
+import { z } from "zod";
 
 type LoaderData = {
   companies: Company[];
@@ -15,8 +21,61 @@ export let loader: LoaderFunction = async () => {
   return json(data);
 };
 
+export let action: ActionFunction = async ({ request }) => {
+  let formData = await request.formData();
+  let formObject = Object.fromEntries(formData.entries());
+  const formSchema = z.object({
+    name: z.string().min(1),
+    remotePolicy: z.enum([
+      RemotePolicy.FULL,
+      RemotePolicy.HYBRID,
+      RemotePolicy.OPTIONAl,
+    ]),
+  });
+
+  let data = formSchema.safeParse(formObject);
+
+  if (!data.success) {
+    return json({ error: data.error.flatten().fieldErrors });
+  }
+
+  await db.company.create({
+    data: { name: data.data.name, remotePolicy: data.data.remotePolicy },
+  });
+  return redirect("/db");
+};
+
 export default function DBTestPage() {
   let data = useLoaderData<LoaderData>();
+  let actionData = useActionData<
+    z.typeToFlattenedError<
+      {
+        name: string;
+        remotePolicy: "FULL" | "HYBRID" | "OPTIONAl";
+      },
+      string
+    >
+  >();
 
-  return <div>{JSON.stringify(data)}</div>;
+  return (
+    <div>
+      {JSON.stringify(data)}
+
+      <Form method="post">
+        <input type={"text"} name="name" />
+        <select name="remotePolicy">
+          {[RemotePolicy.FULL, RemotePolicy.HYBRID, RemotePolicy.OPTIONAl].map(
+            (remotePolicy) => (
+              <option key={remotePolicy} value={remotePolicy}>
+                {remotePolicy}
+              </option>
+            )
+          )}
+        </select>
+        <input type={"submit"} value="CREATE" />
+      </Form>
+
+      {JSON.stringify(actionData)}
+    </div>
+  );
 }
